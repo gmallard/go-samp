6ba29c0923d3dfb0d3b6e7ebc49e1f8ab00d3689
gmallard <allard.guy.m@gmail.com>
2010-10-14 22:58:01 -0400

    Add example for overrides and Array basics.

---
+++ oltesta/oltesta.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+/*
+From examples.  Interesting notes:
+
+a) Function overloads are unsupported :-(.
+b) Arrays are values
+c) Can take address of an Array
+
+*/
+func f(a [3]int) { fmt.Println(a) }
+// func f(a *[3]int) { fmt.Println(a) } // fails to compile
+func fp(a *[3]int) { fmt.Println(a) }
+/*
+*/
+func main() {
+  fmt.Printf("Basic overload tester ....\n")
+	var ar [3] int;
+	f(ar);
+	// passes a copy of ar
+	fp(&ar); // passes a pointer to ar
+}
+
