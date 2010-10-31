d20e5f2d478e76c3c9bbde2a733c27c32e433959
gmallard <allard.guy.m@gmail.com>
2010-10-31 11:38:15 -0400

    Add display of go environment.

--- Makefile
+++ Makefile
@@ -7,6 +7,7 @@ dirs = arrays \
 	maps \
 	methods \
 	oltesta \
+	showgo \
 	slices \
 	struct \
 	ranges \
---
+++ showgo/showgo.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+  fmt.Println("Start....")
+	fmt.Printf("GOROOT: %s\n", runtime.GOROOT())
+	fmt.Printf("Architecture: %s\n", runtime.GOARCH)
+	fmt.Printf("OS: %s\n", runtime.GOOS)
+	fmt.Printf("Version: %s\n", runtime.Version())
+  fmt.Println("End....")
+}
+
