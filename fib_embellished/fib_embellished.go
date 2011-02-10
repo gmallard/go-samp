9e5bd1bb64d0af784c49f3150df747a72898fd04
gmallard <allard.guy.m@gmail.com>
2011-02-10 11:22:33 -0500

    Fibonacci numbers for negative integers.

--- fib_embellished/fib_embellished.go
+++ fib_embellished/fib_embellished.go
@@ -1,7 +1,6 @@
 package main
-
+//
 import "fmt"
-
 /*
 Demonstrate:
 
@@ -9,14 +8,24 @@ a) multiple return values
 b) use of := in functions
 
 */
+//
+//         -6 -5 -4 -3 -2 -1  0  1  2  3  4  5  6
+//     ...,-8, 5,-3, 2,-1, 1, 0, 1, 1, 2, 3, 5, 8,...
+//
 func fib(i int) (result int, flag bool) {
-	if i < 0 {
-		return i, false
+  // The basics:  0 and 1
+	if i == 0 || i == 1 {
+		return i, true
 	}
-	result, flag = i, true
-	if i <= 1 {
-		return
-	}
+  // For negative integers.
+  if i < 0 {
+    posfib, ok := fib(-1 * i)
+    // See formulae at the above reference.
+    if (-1 * i + 1) % 2 != 0 {
+      return -posfib, ok
+    }
+    return posfib, ok
+  }
 	//
 	resm1, flagm1 := fib(i - 1)
 	resm2, flagm2 := fib(i - 2)
@@ -24,10 +33,10 @@ func fib(i int) (result int, flag bool) {
 }
 
 func main() {
-	fmt.Printf("n \tfib(n)\tCheck\n")
-	fmt.Printf("==\t======\t=====\n")
+	fmt.Printf("n tfib(n)tCheckn")
+	fmt.Printf("==t======t=====n")
 	//
-	for ni := -10; ni < 21; ni++ {
+	for ni := -7; ni <= 6; ni++ {
 		result, flag := fib(ni)
 		fmt.Printf("%d\t%d\t%t\n", ni, result, flag)
 	}
--- fib_userop/fib_userop.go
+++ fib_userop/fib_userop.go
@@ -16,13 +16,19 @@ a) Use of user supplied add operation
 
 */
 func fib(i int) (result int, flag bool) {
-	if i < 0 {
-		return i, false
+  // The basics:  0 and 1
+	if i == 0 || i == 1 {
+		return i, true
 	}
-	result, flag = i, true
-	if i <= 1 {
-		return
-	}
+  // For negative integers.
+  if i < 0 {
+    posfib, ok := fib(-1 * i)
+    // See formulae at the above reference.
+    if (-1 * i + 1) % 2 != 0 {
+      return -posfib, ok
+    }
+    return posfib, ok
+  }
 	//
 	resm1, flagm1 := fib(i - 1)
 	resm2, flagm2 := fib(i - 2)
@@ -33,7 +39,7 @@ func main() {
 	fmt.Printf("n \tfib(n)\tCheck\n")
 	fmt.Printf("==\t======\t=====\n")
 	//
-	for ni := -10; ni < 21; ni++ {
+	for ni := -6; ni < 7; ni++ {
 		result, flag := fib(ni)
 		fmt.Printf("%d\t%d\t%t\n", ni, result, flag)
 	}
