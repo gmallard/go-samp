cc1df943f734824147a9b17e8e737a1807cfef2a
gmallard <allard.guy.m@gmail.com>
2011-02-17 09:18:42 -0500

    Embellish string / character length example.

--- stringlens/stringlens.go
+++ stringlens/stringlens.go
@@ -5,11 +5,17 @@ import "fmt"
 func main() {
 	fmt.Println("Start....")
 	//
-	astring := "A\n\u00ffÇ\u754c"
-	fmt.Printf("Len: %d\n", len(astring))
+	astring := "A\n\u00ffÇ\u754c"		// Some UTF-8
+	fmt.Printf("AstringLen: %d\n", len(astring))
 	//
-	for char_index, char := range astring {
-		var cbytes = []uint8(string(char))
+	b1 := astring[1]		// a byte
+	fmt.Printf("b1val: %v\n", b1)
+	//
+	for char_index, char := range astring {		// All characters
+		cstring := string(char)		// Convert a character to a string
+		fmt.Printf("CstringLen: %d\n", len(cstring))	// len = 1..4 bytes
+		var cbytes = []uint8(cstring)									// get the bytes
+		// Info
 		fmt.Printf("%d:%c: Byte Count=%d\n", char_index, char, len(cbytes))
 	}
 	//
