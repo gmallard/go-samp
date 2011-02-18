c715e69dd8dde7b2c002eafe41e7404a236c348f
gmallard <allard.guy.m@gmail.com>
2011-02-18 10:43:46 -0500

    More output information for list example.

--- list/list.go
+++ list/list.go
@@ -9,6 +9,10 @@ import (
 
 func lexamp01() {
 	var myl = list.New()
+	if myl == nil {
+		fmt.Println("is nil")
+	}
+	fmt.Printf("Len: %v\n", myl.Len())
 	fmt.Printf("myl: %v\n", myl)
 	var elts = []string{"elt1", "elt2", "elt3"}
 	for _, nelt := range elts {
