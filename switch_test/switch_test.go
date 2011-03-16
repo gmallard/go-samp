7f4a9eda7329594d49204dc00aad36db309f9c2e
gmallard <allard.guy.m@gmail.com>
2011-03-16 18:58:27 -0400

    Show updated switch test example at go rev 7786.

--- switch_test/switch_test.go
+++ switch_test/switch_test.go
@@ -19,7 +19,20 @@ func main() {
             fmt.Println("Is 2")
         case 3:
             fmt.Println("Is 3")
-        defalt:
+// label was originally coded as:
+// defalt:
+// misspelled.
+// Originally a compile issued no warnings/errors for this.  Which can
+// cause strange and hard to find runtime bugs.  However at go revision
+// 7786, the misspelled coding takes a hard error:
+//
+//* switch_test.go:29: label defalt defined and not used
+//* make[2]: *** [switch_test.6] Error 1
+//
+// Now coded correctly.
+//
+//*ORIG        defalt:
+        default:
             fmt.Println("Dunno")
     }
     fmt.Println("End...")
