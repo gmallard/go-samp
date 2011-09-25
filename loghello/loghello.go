1407bb98974627cf78bc2de7dd862081d9b49d8b
gmallard <allard.guy.m@gmail.com>
2011-09-25 14:03:58 -0400

    Show logging with microseconds.

--- loghello/loghello.go
+++ loghello/loghello.go
@@ -12,7 +12,8 @@ func main() {
 	fmt.Println("Start...")
 	//	var logger = log.New(os.Stdout, nil, 1) // NOT OK
 	//	var logger = log.New(os.Stdout, "LPR ", 1) // OK
-	var logger = log.New(os.Stdout, "LPR ", log.Ldate|log.Ltime|log.Lshortfile) // OK
+	// var logger = log.New(os.Stdout, "LPR ", log.Ldate|log.Ltime|log.Lshortfile) // OK
+	var logger = log.New(os.Stdout, "LPR ", log.Ldate|log.Lmicroseconds|log.Lshortfile) // OK
 	logger.Println("logline")
 	fmt.Println("End...")
 }
