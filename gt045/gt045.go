90c47efd0ffec4e96003b2e1a9c94bae84738e14
gmallard <allard.guy.m@gmail.com>
2012-01-22 12:07:56 -0500

    Add go tour 45 example.  Do not automatically compile it.

---
+++ gt045/gt045.go
@@ -0,0 +1,28 @@
+package main
+
+// go tour #45: http://tour.golang.org/#45
+
+// You need to have the tour installed locally to compile this example.  
+//
+// Install by e.g.:
+//
+// unset GOPATH # YMMV
+// goinstall code.google.com/p/go-tour/gotour
+//
+
+import (
+	"code.google.com/p/go-tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	m := make(map[string]int)
+	for _, v := range strings.Fields(s) {
+		m[v] += 1
+	}
+	return m
+}
+
+func main() {
+	wc.Test(WordCount)
+}
