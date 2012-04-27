e1b2d0bc41da5153f6b5d63f2706eaa2960c3844
gmallard <allard.guy.m@gmail.com>
2012-04-26 20:09:42 -0400

    Embellish sha{1/256} example.

--- shax/shax.go
+++ shax/shax.go
@@ -1,4 +1,4 @@
-// Create a sha1 sum of a string.
+// Create a sha1 and sha256 sums of a string.
 
 package main
 
@@ -10,11 +10,27 @@ import (
 
 func main() {
 	fmt.Println("Start...")
-	s := "Some string A."
+	// String from:
+	// http://en.wikipedia.org/wiki/WebSocket
+	s := "x3JJHMbDL1EzLkh9GBhXDw==258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+
+	// sha1 stuff ...
 	sha1 := sha1.New()
 	sha1.Write([]byte(s))
 	ss := fmt.Sprintf("%x", sha1.Sum(nil))
 	fmt.Printf("%s\n", ss)
+	w := "1d29ab734b0c9585240069a6e4e3e91b61da1969"
+	fmt.Printf("%s\n", w)
+	if ss != w {
+		panic("Uh oh, something is not right")
+	}
+	// ---------------------------------------------------------------------------
+	// The base64 encoding part of that post is left as an exercise for now.
+	// From the article the base64 result should be:
+	// HSmrc0sMlYUkAGmm5OPpG2HaGWk=
+	// *not tested*
+	// ---------------------------------------------------------------------------
+	// sha256 stuff ...
 	sha256 := sha256.New()
 	sha256.Write([]byte(s))
 	ss = fmt.Sprintf("%x", sha256.Sum(nil))
