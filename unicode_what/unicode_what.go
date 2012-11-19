a72323133b8b855280fe9f32c7e095ee0785c611
gmallard <allard.guy.m@gmail.com>
2012-11-18 20:04:12 -0500

    Cedilla example message clarify.

--- unicode_what/unicode_what.go
+++ unicode_what/unicode_what.go
@@ -28,14 +28,15 @@ func main() {
 	fmt.Println()
 
 	// But what about the Ç versus σ thing?
+	// Is Ç really like ς ????
 	v = "Ç"
-	fmt.Println("c-cedilla", v)
-	fmt.Println("c-cedilla UPPER", strings.ToUpper(v))
-	fmt.Println("c-cedilla flip", strings.ToLower(strings.ToUpper(v)))
+	fmt.Println("c-cedillau", v)
+	fmt.Println("c-cedillau UPPER", strings.ToUpper(v))
+	fmt.Println("c-cedillau flip", strings.ToLower(strings.ToUpper(v)))
 	//
 	v = "σ"
-	fmt.Println("sigma", v)
-	fmt.Println("sigma UPPER", strings.ToUpper(v))
-	fmt.Println("sigma flip", strings.ToLower(strings.ToUpper(v)))
+	fmt.Println("sigmau", v)
+	fmt.Println("sigmau UPPER", strings.ToUpper(v))
+	fmt.Println("sigmau flip", strings.ToLower(strings.ToUpper(v)))
 
 }
