1fbce67d60fbdcd83a44b97fd53c5a92c539c9ec
gmallard <allard.guy.m@gmail.com>
2013-07-22 13:53:08 -0400

    Embellish switch example a little.

--- switch_demo/switch_demo.go
+++ switch_demo/switch_demo.go
@@ -9,16 +9,18 @@ import (
 
 func main() {
 	fmt.Println("Start...")
-	anint := 256
-	switch anint {
-	case 1:
-		fmt.Println("Is 1")
-	case 2:
-		fmt.Println("Is 2")
-	case 3:
-		fmt.Println("Is 3")
-	default:
-		fmt.Println("Dunno")
+	i := []int{1, 2, 3, 4, 42}
+	for _, v := range i {
+		switch v {
+		case 1:
+			fmt.Println("Is 1")
+		case 2, 3:
+			fmt.Println("Is 2 or 3")
+		case 4:
+			fmt.Println("Is 4")
+		default:
+			fmt.Println("Default", v)
+		}
 	}
 	fmt.Println("End...")
 }
