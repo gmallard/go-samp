2bdd8fa39bbb6ac5a94095321ced4e1157eb0bd2
gmallard <allard.guy.m@gmail.com>
2016-04-17 10:38:03 -0400

    Tweak log example.

--- loghello/loghello.go
+++ loghello/loghello.go
@@ -14,7 +14,13 @@ func main() {
 	//	var logger = log.New(os.Stdout, nil, 1) // NOT OK
 	//	var logger = log.New(os.Stdout, "LPR ", 1) // OK
 	// var logger = log.New(os.Stdout, "LPR ", log.Ldate|log.Ltime|log.Lshortfile) // OK
-	var logger = log.New(os.Stdout, "LPR ", log.Ldate|log.Lmicroseconds|log.Lshortfile) // OK
-	logger.Println("logline")
+	var logger1 = log.New(os.Stdout, "LPR1 ", log.Ldate|log.Lmicroseconds|log.Lshortfile) // OK
+	var logger2 = log.New(os.Stdout, "LPR2 ", log.Ldate|log.Lmicroseconds|log.Lshortfile) // OK
+
+	logger1.Println("logline L1a")
+	logger2.Println("logline L2a")
+	logger1.Println("logline L1b")
+	logger2.Println("logline L2b")
+
 	fmt.Println("End...")
 }
