6ee3d36f3731a1ac8f8af2c10131627f2ba4ae97
Guy M. Allard <allard.guy.m@gmail.com>
2016-08-02 01:40:27 -0400

    Lock manager demo work:
    - request channel is channel of bool
    - add loop counters

--- channelLock/channelLock.go
+++ channelLock/channelLock.go
@@ -7,15 +7,16 @@ import (
 )
 
 var (
-	sd   = make(chan struct{})
-	req  = make(chan bool)
-	rep  = make(chan bool)
+	sd  = make(chan struct{})
+	req = make(chan chan bool)
+	// rep  = make(chan bool)
 	lock sync.RWMutex
 )
 
 func runner1() {
 	fmt.Println("runner1 starts")
-	q := false
+	var lc = 0
+	var q = false
 	// r := true
 	for {
 		select {
@@ -26,40 +27,72 @@ func runner1() {
 		if q {
 			break
 		}
-		req <- true // Send request
-		_ = <-rep   // Wait for reply (OK)
-		fmt.Println("runner1 run starts")
+		lc++
+		rp := make(chan bool)
+		req <- rp // Send request
+		_ = <-rp  // Wait for reply (OK)
+		fmt.Println("runner1 run starts", lc)
 		time.Sleep(500 * time.Millisecond) // do some work
-		fmt.Println("runner1 run ends")
+		fmt.Println("runner1 run ends", lc)
 	}
-	fmt.Println("runner1 ends")
+	fmt.Println("runner1 ends", lc)
 }
-func lm() {
-	fmt.Println("lm starts")
-	//_ = <-req
-	q := false
+
+func runner2() {
+	fmt.Println("runner2 starts")
+	var lc = 0
+	var q = false
+	// r := true
 	for {
 		select {
 		case _ = <-sd:
 			q = true
-		case _ = <-req:
+		default:
 		}
 		if q {
 			break
 		}
-		fmt.Println("lm have a request")
-		rep <- true
-		fmt.Println("lm sent reply")
+		lc++
+		rp := make(chan bool)
+		req <- rp // Send request
+		_ = <-rp  // Wait for reply (OK)
+		fmt.Println("runner2 run starts", lc)
+		time.Sleep(500 * time.Millisecond) // do some work
+		fmt.Println("runner2 run ends", lc)
 	}
-	fmt.Println("lm ends")
+	fmt.Println("runner2 ends", lc)
+}
+
+func lm() {
+	fmt.Println("lm starts")
+	//_ = <-req
+	var lc = 0
+	var q = false
+	var rp = make(chan bool)
+	for {
+		select {
+		case _ = <-sd:
+			q = true
+		case rp = <-req:
+		}
+		if q {
+			break
+		}
+		lc++
+		fmt.Println("lm have a request", lc)
+		rp <- true
+		fmt.Println("lm sent reply", lc)
+	}
+	fmt.Println("lm ends", lc)
 	//rep <- true
 }
 func main() {
 	//
 	fmt.Printf("%s\n", "hi there")
-	go lm()
-	go runner1()
-	time.Sleep(250 * time.Millisecond)
+	go lm()      // Start lock manager
+	go runner1() // First worker
+	go runner2() // Second worker
+	time.Sleep(1000 * time.Millisecond)
 	// req <- true
 	// _ = <-rep
 	// sd <- true
