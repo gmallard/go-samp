cb1878fe2a9f18e900ad4cf72b9cfa7a27a2953b
Guy M. Allard <allard.guy.m@gmail.com>
2017-07-04 00:09:54 -0400

    Correct twithend example.

--- demo_templates/golangtutorials/twithend/twithend.go
+++ demo_templates/golangtutorials/twithend/twithend.go
@@ -10,12 +10,9 @@ import (
 )
 
 func main() {
-	t := template.Must(template.New("name").Parse("{{with $3 := `hello`}}{{$3}}{{end}}!\n"))
+	t, _ := template.New("test").Parse("{{with `hello`}}{{.}}{{end}}!\n")
 	t.Execute(os.Stdout, nil)
 
-	t1 := template.Must(template.New("name1").Parse("{{with $x3 := `hola`}}{{$x3}}{{end}}!\n"))
+	t1, _ := template.New("test").Parse("{{with `hello`}}{{.}} {{with `Mary`}}{{.}}{{end}}{{end}}!\n") //when nested, the dot takes the value according to closest scope.
 	t1.Execute(os.Stdout, nil)
-
-	t2 := template.Must(template.New("name2").Parse("{{with $x_1 := `hey`}}{{$x_1}} {{.}} {{$x_1}}{{end}}!\n"))
-	t2.Execute(os.Stdout, nil)
 }
