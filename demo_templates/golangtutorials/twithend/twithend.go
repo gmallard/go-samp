777a1a73e306fa7ae86c064a282ea712026a0256
Guy M. Allard <allard.guy.m@gmail.com>
2017-07-04 00:02:08 -0400

    Template with-end example.

---
+++ demo_templates/golangtutorials/twithend/twithend.go
@@ -0,0 +1,21 @@
+/*
+From:
+http://golangtutorials.blogspot.com/2011/10/go-templates-part-2.html
+*/
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+func main() {
+	t := template.Must(template.New("name").Parse("{{with $3 := `hello`}}{{$3}}{{end}}!\n"))
+	t.Execute(os.Stdout, nil)
+
+	t1 := template.Must(template.New("name1").Parse("{{with $x3 := `hola`}}{{$x3}}{{end}}!\n"))
+	t1.Execute(os.Stdout, nil)
+
+	t2 := template.Must(template.New("name2").Parse("{{with $x_1 := `hey`}}{{$x_1}} {{.}} {{$x_1}}{{end}}!\n"))
+	t2.Execute(os.Stdout, nil)
+}
