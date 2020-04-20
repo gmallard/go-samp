cc56c4e1cc41f91fc3c99e5a06c735e097339c10
Guy M. Allard <allard.guy.m@gmail.com>
2020-04-20 10:43:43 -0400

    Better varaible names, add dup. key data.

--- sortex/sortex.go
+++ sortex/sortex.go
@@ -17,27 +17,32 @@ type tt struct {
 var tvs = []tt{
 	tt{6, "xyz1"},
 	tt{2, "xyz4"},
+	tt{19, "xyz7"},
 	tt{19, "xyz3"},
 	tt{7, "xyz2"},
 }
 
+var (
+	sv = tvs
+)
+
 // Type Stringer
 func (v tt) String() string {
-	return fmt.Sprintf("%d: %s", v.tk, v.td)
+	return fmt.Sprintf("%d->%s", v.tk, v.td)
 }
 
-// ByTT stuff.
-type ByTT []tt
+// ByTK stuff.
+type ByTK []tt
 
-func (t ByTT) Len() int {
+func (t ByTK) Len() int {
 	return len(t)
 }
 
-func (t ByTT) Swap(i, j int) {
+func (t ByTK) Swap(i, j int) {
 	t[i], t[j] = t[j], t[i]
 }
 
-func (t ByTT) Less(i, j int) bool {
+func (t ByTK) Less(i, j int) bool {
 	return t[i].tk < t[j].tk
 }
 
@@ -58,20 +63,22 @@ func (t ByTD) Less(i, j int) bool {
 
 func main() {
 	s := "======================================================"
-	sv := tvs
 	//
 	fmt.Println()
 	fmt.Println(s)
+	fmt.Println("Original")
 	fmt.Println(tvs)
 	//
 	fmt.Println()
 	fmt.Println(s)
-	sort.Sort(ByTT(tvs))
+	sort.Sort(ByTK(tvs))
+	fmt.Println("By Key")
 	fmt.Println(tvs)
 	//
 	fmt.Println()
 	fmt.Println(s)
 	sort.Sort(ByTD(tvs))
+	fmt.Println("By Data")
 	fmt.Println(tvs)
 	//
 	fmt.Println()
@@ -80,5 +87,5 @@ func main() {
 	sort.Slice(sv, func(i, j int) bool {
 		return sv[i].tk > sv[j].tk // Note: > ==> Descending
 	})
-	fmt.Println("Desc:", sv)
+	fmt.Println("Desc By Key:", sv)
 }
